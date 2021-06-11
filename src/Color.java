d893f709b44a45f29dbb6add1e68e01293f8d071
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-06-11 13:16:47 +0300

    third task

---
+++ src/Color.java
@@ -0,0 +1,13 @@
+public enum Color {
+
+    RED("#f2463c"),
+    GREEN("#278d39"),
+    BLUE("#00d3ff"),
+    WHITE("#ffffff"),
+    BLACK("#000000");
+    private String hex;
+    Color(String hex){
+        this.hex = hex;
+    }
+
+}
