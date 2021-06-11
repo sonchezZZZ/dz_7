a03ec5138f80bcbf3018cc926b04e7ca261aaeee
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-06-11 14:38:00 +0300

    fourth task

---
+++ src/Collor.java
@@ -0,0 +1,44 @@
+public enum Collor {
+
+    RED(255, 140, 0),
+    GREEN("#8A2BE2"),
+    BLUE("#00d3ff"),
+    WHITE("#ffffff"),
+    BLACK("#000000");
+    private String hex;
+    private int r;
+    private int g;
+    private int b;
+
+    Collor(String hex) {
+        this.hex = hex;
+        this.r = Integer.parseInt(hex.substring(1, 3), 16);
+        this.g = Integer.parseInt(hex.substring(3, 5), 16);
+        this.b = Integer.parseInt(hex.substring(5), 16);
+    }
+
+    Collor(int r, int g, int b) {
+        this.r = r;
+        this.b = b;
+        this.g = g;
+        this.hex = "";
+        int[] colors = new int[]{r, g, b};
+        for (int i : colors) {
+            if (i > 9) {
+                this.hex += Integer.toHexString(i);
+            } else {
+                this.hex += "0";
+                this.hex += Integer.toHexString(i);
+            }
+        }
+    }
+
+    public String getHex() {
+        return hex;
+    }
+
+    public String getRgb() {
+        String rgb = Integer.toString(r) + "," + Integer.toString(g) + "," + Integer.toString(b);
+        return rgb;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,17 +8,21 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         String date = "";
+        String email = " ";
         System.out.println("Print date");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             date = reader.readLine();
+            System.out.println(checkEmail(date));
+            System.out.println("Print email");
+            email = reader.readLine();
+            System.out.println(checkDate(email));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(checkEmail(date));
-//        System.out.println(checkDate(date));
     }
-//    first task
+
+    //    first task
     public static boolean checkDate(String date) {
 //        4 цифры > / > (от 01 до 09 или от 10 до 12) > / > от 0-9 или 10-19 или 20-29 или 30-31
         String regex = "[0-9]{4}\\/((0[1-9])|(1[0-2]))\\/(0[1-9]|[12][0-9]|3[01])\s([01][0-9]|2[0-4]):[0-5][0-9]";
@@ -25,13 +30,13 @@ public class Main {
         return matches;
     }
 
-//    second task
-    public static String checkEmail(String email){
+    //    second task
+    public static String checkEmail(String email) {
         String regex = "(@)(.*\\..*)";
         String domen = "";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
-        if(matcher.find()){
+        if (matcher.find()) {
             domen = matcher.group(2);
         }
         return domen;
